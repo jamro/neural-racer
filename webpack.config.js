b72407cc901a505d2a83728b4378506b265cec45
Krzysztof Jamroz <kjamroz83@gmail.com>
2026-01-15 23:10:55 +0100

    improve bundling

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,88 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/font/[name][ext]',
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? '[name].[contenthash:8].js' : '[name].js',
+      chunkFilename: isProd ? '[name].[contenthash:8].js' : '[name].js',
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'public'),
-          to: path.resolve(__dirname, 'dist/assets'),
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/font/[name][ext]',
+          },
         },
       ],
-    }),
-  ],
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-      {
-        directory: path.join(__dirname, 'public'),
-        publicPath: '/assets',
-      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'public'),
+            to: path.resolve(__dirname, 'dist/assets'),
+            globOptions: {
+              ignore: ['**/.DS_Store'],
+            },
+          },
+        ],
+      }),
     ],
-    compress: true,
-    port: 3000,
-    hot: true,
-    open: true,
-  },
-  mode: 'development',
-  devtool: 'eval-source-map',
+    optimization: {
+      runtimeChunk: 'single',
+      moduleIds: 'deterministic',
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 240_000,
+        cacheGroups: {
+          pixi: {
+            test: /[\\/]node_modules[\\/]pixi\.js[\\/]/,
+            name: 'pixi',
+            chunks: 'all',
+            priority: 30,
+          },
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+            priority: 10,
+          },
+        },
+      },
+    },
+    devServer: {
+      static: [
+        {
+          directory: path.join(__dirname, 'dist'),
+        },
+        {
+          directory: path.join(__dirname, 'public'),
+          publicPath: '/assets',
+        },
+      ],
+      compress: true,
+      port: 3000,
+      hot: true,
+      open: true,
+    },
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? false : 'eval-source-map',
+    performance: isProd ? undefined : { hints: false },
+  };
 };
 
